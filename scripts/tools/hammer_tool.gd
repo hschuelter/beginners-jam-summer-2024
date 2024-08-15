520aefb6d08ffe73185e94cd75555ce7ef2cea72
Arthur <arthur.schuelter@gmail.com>
2024-08-14 21:00:47 -0300

    added animations for hammer and repair

--- scripts/tools/hammer_tool.gd
+++ scripts/tools/hammer_tool.gd
@@ -4,12 +4,15 @@ class_name HammerTool extends Tool
 
 func action() -> void:
 	if can_action:
+		tool_sprite.play("shoot")
+		can_action = false
 		var target = get_global_mouse_position() - self.global_position
-		var melee: Melee = Melee.create_melee(damage, 1, "Melee")
 		
+		await get_tree().create_timer(0.4).timeout
+		
+		var melee: Melee = Melee.create_melee(damage, 1, "Melee")
 		world.add_child(melee)
 		melee.global_position = self.global_position + (target.normalized() * 20)
-		
-		can_action = false
 		action_timer.start()
+		
 
--- scripts/tools/repair_tool.gd
+++ scripts/tools/repair_tool.gd
@@ -6,11 +6,15 @@ var wall: Wall
 
 func action() -> void:
 	if can_action:
+		tool_sprite.play("shoot")
+		can_action = false
 		var target = get_global_mouse_position() - self.global_position
+		
+		await get_tree().create_timer(0.4).timeout
+		
 		var melee_repair: MeleeRepair = MeleeRepair.create_melee_repair(repair, 1, "Melee Repair")
 		
 		world.add_child(melee_repair)
 		melee_repair.global_position = self.global_position + (target.normalized() * 20)
 		
-		can_action = false
 		action_timer.start()
