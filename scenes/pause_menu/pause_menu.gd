2113078c6e6e0597aa9452c9ae22fd23b015bd25
PscheidtLucas <163616249+PscheidtLucas@users.noreply.github.com>
2024-08-27 16:53:54 -0300

    ajustes feitos

--- scenes/pause_menu/pause_menu.gd
+++ scenes/pause_menu/pause_menu.gd
@@ -1,14 +1,12 @@
 extends Control
 
 var paused = false
+const MAIN_MENU_SCENE : PackedScene = preload("res://scenes/main_menu.tscn")
 
 func _on_resume_pressed():
 	self.hide()
 	get_tree().paused = false
 	paused = false
-
-func _on_quit_pressed():
-	get_tree().quit()
 
 func pauseMenu():
 	if paused:
@@ -23,3 +21,16 @@ func pauseMenu():
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
 		pauseMenu()
+
+
+func _on_restart_pressed():
+	paused = false
+	get_tree().paused = false
+	self.hide()
+	get_tree().reload_current_scene()
+
+
+func _on_mainmenu_pressed():
+	paused = false
+	get_tree().paused = false
+	get_tree().change_scene_to_packed(MAIN_MENU_SCENE)
